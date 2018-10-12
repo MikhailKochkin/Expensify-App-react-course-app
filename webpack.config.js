88e92d02822161b711eaeeea141ff641f81902c9
Михаил Кочкин <mihailkockin@MacBook-Pro-Mihail.local>
2018-10-12 11:29:31 +0300

    Setup devDependencies

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,17 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = (env) => {
-  const isProduction = env === 'production'
-  const CSSExtract= new ExtractTextPlugin('styles.css');
+  const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
 
   return {
     entry: './src/app.js',
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
     },
     module: {
-      rules:[{
+      rules: [{
         loader: 'babel-loader',
         test: /\.js$/,
         exclude: /node_modules/
@@ -31,7 +31,7 @@ module.exports = (env) => {
               options: {
                 sourceMap: true
               }
-            },
+            }
           ]
         })
       }]
@@ -42,7 +42,8 @@ module.exports = (env) => {
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
-      historyApiFallback: true
+      historyApiFallback: true,
+      publicPath: '/dist/'
     }
-  }
-}
+  };
+};
